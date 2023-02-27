af526b8232c882fea1c8cd14f88b1650ac3636c8
Issou83 <issou8@gmail.com>
2023-02-27 20:18:35 +0100

    recuperation de la saisi de l'input pour injection en tant que parametre URL, ce qui permet d'effectuer une recherche de données par type de plat + retouche css

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Cards from './components/Cards';
 
 const App = () => {
   const [mealsData, setMealsData] = useState([])
-
+  const [inputSearch, setInputSearch] = useState("")
   useEffect (() => {
-    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    axios
+    .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
     .then((res) => setMealsData(res.data.meals))
-  })
+  },[inputSearch]);
 
 
   return (
     <div className='app-container'>
       <h1>Cookig App</h1>
-      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)"/>
+      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)"
+      onChange={(e) => setInputSearch(e.target.value)}/>
       <div className='meals-container'>
         {mealsData.map((meal) => (
           <Cards key={meal.idMeal} meal={meal} />
